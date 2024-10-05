a4088f7811c81eda274973a23b6b60a569126abb
fanhl <dyguests@qq.com>
2024-10-05 19:10:49 +0800

    add two race

--- Assets/Scripts/Wars/ScriptableObjects/RaceSo.cs
+++ Assets/Scripts/Wars/ScriptableObjects/RaceSo.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
+using Wars.Views;
 
 namespace Wars.ScriptableObjects
 {
     [CreateAssetMenu(fileName = "RaceSo", menuName = "Koyou/RaceSo", order = 0)]
     public class RaceSo : ScriptableObject
     {
-        
+        public MainBaseView mainBaseViewPfb;
     }
 }
