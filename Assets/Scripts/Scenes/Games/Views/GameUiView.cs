69192c2faa0c213b27e503e236c71c4256642e50
fanhl <dyguests@qq.com>
2024-10-06 15:55:21 +0800

    reactive

--- Assets/Scripts/Scenes/Games/Views/GameUiView.cs
+++ Assets/Scripts/Scenes/Games/Views/GameUiView.cs
@@ -5,12 +5,17 @@ namespace Scenes.Games.Views
     public class GameUiView : MonoBehaviour
     {
         #region GameUiView
+        
+        [SerializeField]
+        private ResourcesPanelView resourcesPanelView;
 
         private PlayerCtlr _playerCtlr;
 
         public void BindPlayer(PlayerCtlr playerCtlr)
         {
             _playerCtlr = playerCtlr;
+            
+            resourcesPanelView.BindPlayer(playerCtlr);
         }
 
         #endregion
--- Assets/Scripts/Scenes/Games/Views/ResourcesPanelView.cs
+++ Assets/Scripts/Scenes/Games/Views/ResourcesPanelView.cs
@@ -4,6 +4,21 @@ namespace Scenes.Games.Views
 {
     public class ResourcesPanelView : MonoBehaviour
     {
-        
+        #region ResourcesPanelView
+
+        [SerializeField] private ResourcePanelView foodRpv;
+        [SerializeField] private ResourcePanelView populationRpv;
+
+        private PlayerCtlr _playerCtlr;
+
+        public void BindPlayer(PlayerCtlr playerCtlr)
+        {
+            _playerCtlr = playerCtlr;
+
+            var faction = _playerCtlr.Faction;
+            
+        }
+
+        #endregion
     }
 }
